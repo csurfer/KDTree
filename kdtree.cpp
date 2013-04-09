8f95b42d7f940036014029d34d4b2a319b41d134
Vishwas <sharma.vishwas88@gmail.com>
2013-04-09 14:21:18 -0400

    kdtree.cpp file inclusion

---
+++ kdtree.cpp
@@ -0,0 +1,37 @@
+/* 
+ * Author : Vishwas B Sharma
+ * File   : kdtree.cpp
+ */
+#include<vector>
+#include<stack>
+#include<set>
+#include<list>
+#include<map>
+#include<queue>
+#include<deque>
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<cassert>
+#include<cstdlib>
+#include<cstdio>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+#define INF (int)1e9
+#define LINF (long long)1e18
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define ABS(x) ((x)<0?(-(x)):(x))
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define REP(i,n) FOR(i,0,n)
+#define foreach(v,c) for( typeof((c).begin()) v = (c).begin();  v != (c).end(); ++v)
+
+int main()
+{
+  return 0;
+}
+
